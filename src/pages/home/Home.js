c51c33a94c7f37f1b69f1f3cee87b55363399e26
FlaceyFurr <flaceyfurr@gmail.com>
2023-01-24 12:57:40 +0200

    Made the main page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,24 @@
 import "./style.css";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <main className="home-section">
-      <div></div>
+      <div className="container">
+        <div className="home__text">
+          <h5 className="home__text-subheading">so, you want to travel to</h5>
+          <h1 className="home__text-title">space</h1>
+          <p className="home__text-subtitle">
+            Let's face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we'll give ou a truly out of this
+            world experience!
+          </p>
+        </div>
+        <NavLink to="/destination" className="button-link">
+          Explore
+        </NavLink>
+      </div>
     </main>
   );
 };
