6f015931273cef28ec83e42015510f8b75722bd4
FlaceyFurr <flaceyfurr@gmail.com>
2023-01-25 11:56:44 +0200

    Added helpers list

---
+++ src/helpers/crewList.js
@@ -0,0 +1,37 @@
+import portrait01 from "./../images/crew/image-douglas-hurley.png";
+import portrait02 from "./../images/crew/image-mark-shuttleworth.png";
+import portrait03 from "./../images/crew/image-victor-glover.png";
+import portrait04 from "./../images/crew/image-anousheh-ansari.png";
+
+const crew = [
+  {
+    id: 0,
+    img: portrait01,
+    status: "commander",
+    name: "douglas hurley",
+    description: "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+  },
+  {
+    id: 1,
+    img: portrait02,
+    status: "mission specialist",
+    name: "mark shuttleworth",
+    description: "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
+  },
+  {
+    id: 2,
+    img: portrait03,
+    status: "pilot",
+    name: "victor glover",
+    description: "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
+  },
+  {
+    id: 3,
+    img: portrait04,
+    status: "flight engineer",
+    name: "anounsheh ansari",
+    description: "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
+  },
+];
+
+export default crew;
---
+++ src/helpers/technologyList.js
@@ -0,0 +1,34 @@
+import tech01 from "./../images/technology/image-launch-vehicle-portrait.jpg";
+import tech01Mobile from "./../images/technology/image-launch-vehicle-landscape.jpg";
+import tech02 from "./../images/technology/image-spaceport-portrait.jpg";
+import tech02Mobile from "./../images/technology/image-spaceport-landscape.jpg";
+import tech03 from "./../images/technology/image-space-capsule-portrait.jpg";
+import tech03Mobile from "./../images/technology/image-space-capsule-landscape.jpg";
+
+
+
+const technology = [
+  {
+    id: 0,
+    img: tech01,
+    imgMobile: tech01Mobile,
+    title: "launch vehicle",
+    subtitle: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+  },
+  {
+    id: 1,
+    img: tech02,
+    imgMobile: tech02Mobile,
+    title: "spaceport",
+    subtitle: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+  },
+  {
+    id: 2,
+    img: tech03,
+    imgMobile: tech03Mobile,
+    title: "space capsule",
+    subtitle: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+  },
+];
+
+export default technology;
