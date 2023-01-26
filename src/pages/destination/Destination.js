84cb5ea4e126253aaacc085edd33b5886dec1f68
FlaceyFurr <flaceyfurr@gmail.com>
2023-01-26 21:54:09 +0200

    Added switching destination items

--- src/pages/destination/Destination.js
+++ src/pages/destination/Destination.js
@@ -1,34 +1,32 @@
+import React, { useState } from "react";
 import "./style.css";
-import moon from "./../../images/destination/image-moon.png";
+import destination from "../../helpers/destinationList";
 
 const Destination = () => {
+  const [dest, setDest] = useState(destination);
+  const [selection, setSelection] = useState(0);
+
   return (
     <main className="destination-section">
       <h5 className="destination__title">
         <span className="bold number">01</span> pick your destination
       </h5>
       <div className="container">
-        <img src={moon} alt="moon" className="image" />
+        <img src={dest[selection].img} alt="moon" className="image" />
         <div className="info">
           <ul className="info__list">
-            <li className="info__list-item">moon</li>
-            <li className="info__list-item">mars</li>
-            <li className="info__list-item">europa</li>
-            <li className="info__list-item">titan</li>
+            {dest?.map((item, index) => (
+              <button onClick={() => setSelection(index)}>{item.title}</button>
+            ))}
           </ul>
-          <h2 className="info__title">Moon</h2>
-          <p className="info__subtitle">
-            See our planet as you’ve never seen it before. A perfect relaxing
-            trip away to help regain perspective and come back refreshed. While
-            you’re there, take in some history by visiting the Luna 2 and Apollo
-            11 landing sites.
-          </p>
+          <h2 className="info__title">{dest[selection].title}</h2>
+          <p className="info__subtitle">{dest[selection].subtitle}</p>
           <hr className="info__line" />
           <div className="subheading">
             <h5 className="subheading__title">avg. distance</h5>
             <h5 className="subheading__title">est. travel time</h5>
-            <p className="subheading__subtitle">384,000 km</p>
-            <p className="subheading__subtitle">3 days</p>
+            <p className="subheading__subtitle">{dest[selection].distance}</p>
+            <p className="subheading__subtitle">{dest[selection].travelTime}</p>
           </div>
         </div>
       </div>
